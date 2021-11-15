f4d594fb8b5aace9c23108de1ca141ecc96d57f2
daniel19066 <dpallaresr@unal.edu.co>
2021-11-14 20:00:22 -0500

    footer ahi mas o menos

--- front_end/lib/src/View/pages/home/prueba.dart
+++ front_end/lib/src/View/pages/home/prueba.dart
@@ -8,6 +8,13 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
+  int  _indexseleccionado=0;
+  PageController pageController= new PageController();
+  void _onItemTapped(int index) {
+    setState(() {
+      _indexseleccionado = index;
+    });
+  }
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
@@ -26,7 +33,18 @@ class MapSampleState extends State<MapSample> {
           _controller.complete(controller);
         },
       ),
-
+      bottomNavigationBar: BottomNavigationBar(items: const<BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home),
+        label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Perfil'),
+        BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined), label: 'Carrro'),
+        BottomNavigationBarItem(icon: Icon(Icons.message_rounded), label: 'Mensajes')
+      ],
+        currentIndex: _indexseleccionado,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
     );
   }
 
